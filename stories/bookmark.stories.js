990bd25619fd548390f91b1ca8cf4e0b1dd5e887
macfrei <lene.frei@neuefische.de>
2020-07-23 11:15:35 +0200

    Add bookmark story and style

---
+++ stories/bookmark.stories.js
@@ -0,0 +1,9 @@
+import '../styles.css'
+
+export default { title: 'Components/bookmark' }
+
+export const bookmarked = () =>
+  '<section class="card"><i class="card__bookmark material-icons md-36 md-dark">bookmark</i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic temporeoptio, quam dicta mollitia voluptatem ipsa pariatur possimus?</section>'
+
+export const notBookmarked = () =>
+  '<section class="card"><i class="card__bookmark material-icons md-36 md-dark md-inactive" >bookmark</i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic temporeoptio, quam dicta mollitia voluptatem ipsa pariatur possimus?</section>'
--- stories/tags.stories.js
+++ stories/tags.stories.js
@@ -6,3 +6,6 @@ export const simpleTag = () => '<span class="card__tag">Tag1</span>'
 
 export const tagWithBackground = () =>
   '<section class="card" style="background: yellow;"><span class="card__tag">tag123</span></section>'
+
+export const threeTags = () =>
+  '<span class="card__tag">tag1</span><span class="card__tag">tag2</span><span class="card__tag">tag3</span>'
